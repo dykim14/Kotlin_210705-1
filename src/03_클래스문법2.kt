d503f6b8106174fa87b38082fcdca16cccc4e21a
Chansik Yun <chansigi@gmail.com>
2021-07-05 12:05:12 +0900

    .

--- src/03_클래스문법2.kt
+++ src/03_클래스문법2.kt
@@ -27,10 +27,18 @@ fun main() {
     val b = 0xff0000 // Red
     val c = "Suwon"
 
+    // 문제점
+    // - 어떤 값이 어떤 파라미터로 전달되는지 알기 어렵다.
+    // 해결방법
+    // - 파라미터 지정 호출
+    /*
     car.go()
     car.go(a)
     car.go(a, b)
     car.go(a, b, c)
+    */
+
+    car.go(speed = a, color = b, destination = c)
 }
 
 
