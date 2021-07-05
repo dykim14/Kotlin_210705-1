660aa2c3e3107a351efa702a2023abef0ace30a1
Chansik Yun <chansigi@gmail.com>
2021-07-05 16:46:46 +0900

    .

--- src/07_봉인클래스.kt
+++ src/07_봉인클래스.kt
@@ -1,6 +1,8 @@
 // 07_봉인클래스.kt
 package ex7
 
+import java.lang.Exception
+
 // 1. enum class
 // 2. enum은 자바와 동일하게 프로퍼티와 메소드를 가질 수 있는 객체 타입입니다.
 /*
@@ -34,6 +36,32 @@ enum class Color(
 //  - Statement은 결과가 존재하지 않습니다.
 //  - Expression은 결과가 존재합니다.
 
+// when
+// - 객체 동등성 비교
+fun mix(c1: Color, c2: Color): Color {
+    val set: Set<Color> = setOf(c1, c2)
+    return when (set) {
+        setOf(Color.RED, Color.YELLOW) -> Color.ORANGE
+        else -> throw Exception("Unknown")
+    }
+}
+
+fun main() {
+    val result1 = mix(Color.RED, Color.YELLOW)
+    val result2 = mix(Color.YELLOW, Color.RED)
+    println(result1)
+    println(result2)
+
+
+    val color = Color.BLUE
+    println(color.green)
+
+    println(color.rgb())
+
+    val name = getWarmth(color)
+    println(name)
+}
+
 fun getWarmth(color: Color): String {
     return when (color) {
         Color.RED, Color.ORANGE, Color.YELLOW -> "Warm"
@@ -59,12 +87,3 @@ fun getName(color: Color): String {
     }
 }
 
-fun main() {
-    val color = Color.BLUE
-    println(color.green)
-
-    println(color.rgb())
-
-    val name = getWarmth(color)
-    println(name)
-}
