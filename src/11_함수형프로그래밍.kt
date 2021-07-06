d15c627c888e1e722f2804fe873c3a8ddb95d2bc
Chansik Yun <chansigi@gmail.com>
2021-07-06 15:07:59 +0900

    .

--- src/11_함수형프로그래밍.kt
+++ src/11_함수형프로그래밍.kt
@@ -17,4 +17,40 @@ fun add1(a: Int, b: Int): Int {
     return a + b
 }
 
-fun add2(a: Int, b: Int): Int = a + b
+// fun add2(a: Int, b: Int): Int = a + b
+fun add2(a: Int, b: Int) = a + b
+// > 반환 타입의 생략이 가능합니다.
+//   함수의 반환 타입에 대한 추론이 가능합니다.
+//   - 함수가 제네릭 기반으로 되어 있다면, 타입 추론이 가능합니다.
+
+// var a: Int = 10
+// var a = 10
+
+// 2. 함수 타입
+//   코틀린에서는 함수의 타입은 함수의 인자의 정보, 반환 타입에 의해서 결정됩니다.
+//  => 인자와 반환 타입이 동일한 함수는 같은 타입의 함수 입니다.
+//  => 함수의 시그니처(인자 타입과 반환 타입 정보)가 동일하면, 동일한 타입입니다.
+
+// 3. 전역 함수는 :: 을 통해 참조할 수 있습니다.
+// 4. 함수 타입 표현 방법
+//    (Int, Int) -> Int
+
+fun foo(a: Int, b: Char, c: String): Double = 3.14
+
+fun main() {
+    var fn1: (Int, Int) -> Int = ::add1
+    fn1 = ::add2
+    fn1(10, 20)
+    
+    val fn2: (Int, Char, String) -> Double = ::foo
+    foo(42, 'a', "Hello")
+
+
+    val result1 = add1(10, 20)
+    val result2 = add2(10, 20)
+}
+
+
+
+
+
