3e7b626d768ea121eb84d70c03c3f5f926e1d6ae
Chansik Yun <chansigi@gmail.com>
2021-07-06 09:08:42 +0900

    .

--- src/07_봉인클래스2.kt
+++ src/07_봉인클래스2.kt
@@ -36,7 +36,6 @@ sealed class Expr
 class Num(val value: Int) : Expr()
 class Sum(val left: Expr, val right: Expr) : Expr()
 */
-
 sealed interface Expr
 class Num(val value: Int) : Expr
 class Sum(val left: Expr, val right: Expr) : Expr
