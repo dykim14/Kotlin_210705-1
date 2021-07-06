5112f175e062698c23f3e16a1cb1189823778e8e
Chansik Yun <chansigi@gmail.com>
2021-07-06 17:10:41 +0900

    .

--- src/15_고차함수.kt
+++ src/15_고차함수.kt
@@ -17,6 +17,13 @@ package ex15
 //       |
 // MutableMap<K, V>
 
+// 문제점
+//  : 변하지 않는 전체 알고리즘에서 변해야 하는 정책은 분리되어야 한다.
+//    "공통성과 가변성의 분리"
+
+// Kotlin
+//  1) Java    - 인터페이스 기반 설계
+//  2) Kotlin  - 함수 기반 설계
 fun filterOdds(data: List<Int>): List<Int> {
     // val result = emptyList<Int>()
     val result = mutableListOf<Int>()
@@ -30,9 +37,25 @@ fun filterOdds(data: List<Int>): List<Int> {
     return result
 }
 
+fun filterEvens(data: List<Int>): List<Int> {
+    val result = mutableListOf<Int>()
+
+    for (e in data) {
+        if (e % 2 == 0) {
+            result.add(e)
+        }
+    }
+
+    return result
+}
+
 
 fun main() {
     val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
+    val result1 = filterOdds(list)
+    val result2 = filterEvens(list)
 
+    println(result1)
+    println(result2)
 }
