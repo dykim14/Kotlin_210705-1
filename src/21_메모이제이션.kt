0a2c9fc3eaa68eaeebc220d3bdc23da4d5812364
Chansik Yun <chansigi@gmail.com>
2021-07-07 16:10:43 +0900

    .

---
+++ src/21_메모이제이션.kt
@@ -0,0 +1,37 @@
+// 21_메모이제이션.kt
+package ex21
+
+// 함수의 결과를 저장해서, 재활용하는 최적화
+// "메모이제이션"
+//   순수 함수(pure function)
+//    : 인자가 동일하면, 결과도 동일하다.
+//    => 참조적 투명성
+
+// when expresion을 이용해서, 코드를 작성하면 편리합니다.
+/*
+fun fib(k: Int): Long = when (k) {
+    0, 1 -> 1
+    else -> fib(k - 1) + fib(k - 2)
+}
+*/
+
+val cache = mutableMapOf<Int, Long>()
+fun fib(k: Int): Long = when (k) {
+    0, 1 -> 1
+    else -> {
+        val result = cache[k]
+        if (result != null)
+            result
+        else {
+            val n = fib(k - 1) + fib(k - 2)
+            cache[k] = n
+            n
+        }
+    }
+}
+
+fun main() {
+    val result = fib(100)
+
+    println(result)
+}
