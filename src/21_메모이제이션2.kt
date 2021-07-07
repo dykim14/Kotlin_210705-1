d3698d46095d2ed117b4da27a76ada155eeaeca4
Chansik Yun <chansigi@gmail.com>
2021-07-07 16:20:34 +0900

    .

--- src/21_메모이제이션2.kt
+++ src/21_메모이제이션2.kt
@@ -16,14 +16,22 @@ class Car(
     val speed: Int
 )
 
+fun <T> T.toJSON(): String {
+    val gson = Gson()
+    return gson.toJson(this)
+}
+
 fun main() {
     val user = User("Tom", 42, "Suwon", 1)
     val car = Car("BMW", "Sedan", 100)
 
     // Gson
-    val gson = Gson()
-    val json1 = gson.toJson(user)
-    val json2 = gson.toJson(car)
+    // val gson = Gson()
+    // val json1 = gson.toJson(user)
+    // val json2 = gson.toJson(car)
+
+    val json1 = user.toJSON()
+    val json2 = car.toJSON()
 
     println(json1)
     println(json2)
