acd8a4beb7da81dc257635378c79f5a643e1110b
Chansik Yun <chansigi@gmail.com>
2021-07-07 14:00:23 +0900

    .

--- src/18_인라인2.kt
+++ src/18_인라인2.kt
@@ -1,19 +1,19 @@
 // 18_인라인2.kt
 package ex18_2
 
-class Intent
+class Intent(context: Activity, clazz: Class<out Activity>)
 
 open class Activity {
     open fun onCreate() {}
 
-    fun startActivity(intent: Intent, clazz: Class<out Activity>) {}
+    fun startActivity(intent: Intent) {}
 }
 
 class MainActivity : Activity() {
     override fun onCreate() {
         super.onCreate()
 
-        // ...
+        
 
     }
 }
