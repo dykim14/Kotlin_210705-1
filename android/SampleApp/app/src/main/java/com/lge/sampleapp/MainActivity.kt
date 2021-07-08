cdb1da8e8af592f765c25f0bac373b9f3f720a7c
Chansik Yun <chansigi@gmail.com>
2021-07-08 14:53:40 +0900

    .

--- android/SampleApp/app/src/main/java/com/lge/sampleapp/MainActivity.kt
+++ android/SampleApp/app/src/main/java/com/lge/sampleapp/MainActivity.kt
@@ -9,6 +9,17 @@ import android.os.Bundle
 //         classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
 
 //   - app module level: build.gradle
+//     : id 'kotlin-android'
+//
+//    kotlinOptions {
+//        jvmTarget = '1.8'
+//    }
+//
+//    implementation "org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version"
+//     : 코틀린 표준 라이브러리에 대한 의존성입니다.
+//
+//    implementation 'androidx.core:core-ktx:1.6.0'
+//     : "Android Jetpack"이 제공하는 코틀린 확장 라이브러리 입니다.
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
