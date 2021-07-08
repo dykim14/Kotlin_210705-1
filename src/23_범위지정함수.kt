df21765ced4f00d3ff36ddb5dfcefa4de4d77a1c
Chansik Yun <chansigi@gmail.com>
2021-07-08 09:47:48 +0900

    .

--- src/23_범위지정함수.kt
+++ src/23_범위지정함수.kt
@@ -10,17 +10,44 @@ fun alphabet1(): String {
     for (letter in 'A'..'Z')
         result.append(letter)
 
-    return readLine().toString()
+    return result.toString()
 }
 
 // 2. apply
 //  : 인자가 this로 전달되는 람다 => 수신 객체 지정 람다
-fun alphabet2(): String {
+//    (T) -> Unit
+//    - 첫번째 인자가 it 으로 전달됩니다.
+//   T.() -> Unit
+//    - 첫번째 인자가 this 로 전달됩니다.
+
+// apply 구현입니다.
+//  : 객체의 초기화 과정을 블록을 통해 캡슐화할 때 사용합니다.
+//   - 빌더 등의 초기화 과정이 복잡한 객체에서 사용합니다.
+inline fun <T> T.apply3(block: T.() -> Unit): T {
+    // block(this)
+    // this.block()
+    block()
+    return this
+}
+
+fun alphabet_apply2(): String {
     return StringBuilder().apply {
         for (letter in 'A'..'Z')
             append(letter)
     }.toString()
 }
+
+fun alphabet3(): String = StringBuilder().apply {
+    for (letter in 'A'..'Z')
+        append(letter)
+}.toString()
+
+// buildString
+fun alphabet_buildString(): String = buildString {
+    for (letter in 'A'..'Z')
+        append(letter)
+}
+
 
 fun main() {
     val result = alphabet1()
