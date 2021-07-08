d665fc9008d006ac0feac8dca33e35476755b990
Chansik Yun <chansigi@gmail.com>
2021-07-08 11:46:51 +0900

    .

--- src/25_컬렉션.kt
+++ src/25_컬렉션.kt
@@ -1,3 +1,32 @@
 // 25_컬렉션.kt
 package ex25
 
+class SListIterator<E>(
+    var current: SList.Node<E>?
+) : Iterator<E> {
+
+    override fun hasNext() = current != null
+
+    override fun next(): E {
+        // 현재의 값이 존재하지 않을 경우, 종료되어야 합니다.
+        val ret = current!!.value
+        current = current?.next
+
+        return ret
+    }
+}
+
+class SList<E> : Iterable<E> {
+    class Node<E>(val value: E, val next: Node<E>?)
+
+    private var head: Node<E>? = null
+
+    val front: E?
+        get() = head?.value
+
+    fun addFront(value: E) {
+        head = Node(value, head)
+    }
+
+    override fun iterator() = SListIterator(head)
+}
--- src/javasample/ex25/Sample.java
+++ src/javasample/ex25/Sample.java
@@ -11,8 +11,13 @@ package javasample.ex25;
 //         : 현재 요소의 값을 반환하고, 다음 위치로 이동한다.
 
 //   2) Iterable<E> : SList<E>
+//      Iterator<T> iterator();
+//        : 컬렉션을 순회하기 위한 반복자 객체를 반환합니다.
 
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
 
 class SListIterator<E> implements Iterator<E> {
     private SList.Node<E> current;
@@ -36,7 +41,12 @@ class SListIterator<E> implements Iterator<E> {
 }
 
 // 단일 연결 리스트
-class SList<E> {
+class SList<E> implements Iterable<E> {
+    @Override
+    public Iterator<E> iterator() {
+        return new SListIterator<>(head);
+    }
+
     static class Node<E> {
         private E value;
         private Node<E> next;
@@ -68,11 +78,31 @@ class SList<E> {
 
 public class Sample {
     public static void main(String[] args) {
-        SList<Integer> s = new SList<>();
-        s.addFront(10);
-        s.addFront(20);
-        s.addFront(30);
+        // SList<Integer> s = new SList<>();
+        // s.addFront(10);
+        // s.addFront(20);
+        // s.addFront(30);
+        // System.out.println(s.front());
 
-        System.out.println(s.front());
+        List<Integer> s = Arrays.asList(10, 20, 30);
+
+        Iterator<Integer> iterator = s.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        // Iterator 패턴을 구현한 컬렉션에 대해서
+        // 향상된 for 구문을 사용할 수 있습니다.
+        for (Integer e : s) {
+            System.out.println(e);
+        }
+
+        // Java 8 에서는 많은 기능이 인터페이스의 기본 구현을 통해 제공됩니다.
+        s.forEach(e -> {
+            System.out.println(e);
+        });
+
+        // Java 8 - Method Reference
+        s.forEach(System.out::println);
     }
 }
