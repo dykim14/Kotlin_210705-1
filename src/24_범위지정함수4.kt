a7ae3c412d101b1992d14b088b7474629f28b040
Chansik Yun <chansigi@gmail.com>
2021-07-08 10:45:46 +0900

    .

--- src/24_범위지정함수4.kt
+++ src/24_범위지정함수4.kt
@@ -1,1 +1,43 @@
 package ex24
+
+import java.io.Closeable
+import java.io.FileInputStream
+
+// 코틀린은 Try-with-resources 문법이 존재하지 않습니다.
+// => use
+
+// > 비메모리 자원(파일 등)에 대해서는 명시적인 종료 메소드를 호출해야 합니다.
+// 사용자가 작성한 클래스가 비메모리 자원을 이용하고 있다면,
+// 명시적인 종료 메소드를 제공해야 한다.
+class MyResource : Closeable {
+    override fun close() {
+        println("자원 해지")
+    }
+
+    fun process() {
+        println("자원 사용")
+    }
+
+    init {
+        println("자원 할당")
+    }
+}
+
+fun main() {
+    val resource = MyResource()
+
+    resource.use {
+        resource.process()
+    }
+}
+
+
+/*
+fun main() {
+    val fis = FileInputStream("a.txt")
+    val n = fis.read()
+    println(n)
+
+    fis.close()
+}
+*/
--- src/javasample/ex24/Sample.java
+++ src/javasample/ex24/Sample.java
@@ -1,12 +1,38 @@
 package javasample.ex24;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-// 비메모리 자원(파일 등)에 대해서는 명시적인 종료 메소드를 호출해야 합니다.
+// > 비메모리 자원(파일 등)에 대해서는 명시적인 종료 메소드를 호출해야 합니다.
+
+// 사용자가 작성한 클래스가 비메모리 자원을 이용하고 있다면,
+// 명시적인 종료 메소드를 제공해야 한다.
+
+// Try with resources 문법은 AutoCloseable 인터페이스를 구현해야 합니다.
+class MyResource implements AutoCloseable {
+    public MyResource() {
+        System.out.println("자원 할당");
+    }
+
+    @Override
+    public void close() {
+        System.out.println("자원 해지");
+    }
+
+    public void process() {
+        System.out.println("자원 사용");
+    }
+}
+
 
 public class Sample {
+    public static void main(String[] args) {
+        try (MyResource resource = new MyResource()) {
+            resource.process();
+        }
+    }
+
+
     /*
     public static void main(String[] args) throws Exception {
         FileOutputStream fos = new FileOutputStream("a.txt");
@@ -16,6 +42,18 @@ public class Sample {
     }
     */
 
+    // Java 7 - Try with Resources
+    /*
+    public static void main(String[] args) {
+        try (FileOutputStream fos = new FileOutputStream("a.txt")) {
+            fos.write(42);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    */
+
+    /*
     public static void main(String[] args) {
         FileOutputStream fos = null;
         try {
@@ -33,8 +71,6 @@ public class Sample {
                 }
             }
         }
-
-
-
     }
+    */
 }
