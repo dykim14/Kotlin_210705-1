2cb349a706942aa69c8206fc9e574e0b84f0cfc2
Chansik Yun <chansigi@gmail.com>
2021-07-08 14:48:54 +0900

    .

--- android/SampleApp/app/src/main/java/com/lge/sampleapp/MainActivity.kt
+++ android/SampleApp/app/src/main/java/com/lge/sampleapp/MainActivity.kt
@@ -3,6 +3,13 @@ package com.lge.sampleapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// 1. build.gradle
+//   - project level: build.gradle
+//     :   ext.kotlin_version = "1.5.20"
+//         classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+
+//   - app module level: build.gradle
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
