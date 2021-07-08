325b45e1bcfca260ec9548794d368b5180bdaf80
Chansik Yun <chansigi@gmail.com>
2021-07-08 17:20:11 +0900

    .

--- android/SampleApp/app/src/main/java/com/lge/sampleapp/MainActivity2.kt
+++ android/SampleApp/app/src/main/java/com/lge/sampleapp/MainActivity2.kt
@@ -8,6 +8,7 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
 import com.lge.sampleapp.databinding.MainFragmentBinding
 
 class MainActivity2 : AppCompatActivity() {
@@ -21,10 +22,25 @@ class MainActivity2 : AppCompatActivity() {
         if (savedInstanceState == null) {
             val fragment = MainFragment()
 
+            /*
             supportFragmentManager
                 .beginTransaction()
                 .replace(R.id.fragmentContainer, fragment)
                 .commit()
+           */
+
+            // Kotlin 에서 안드로이드를 사용할 때
+            // 편리하게 사용할 수 있는 Kotlin Android Extension 라이브러리가 있습니다.
+            //    Jetpack Android KTX
+            //  > 의존성 추가
+            //    androidx.fragment:fragment-ktx:1.3.5
+
+            supportFragmentManager.commit {
+                replace(R.id.fragmentContainer, fragment)
+            }
+
+
+
         }
     }
 }
@@ -109,7 +125,6 @@ class MainFragment : Fragment(R.layout.main_fragment) {
 
         // 프로퍼티로 바인딩 객체를 사용하지 않는다면,
         // 메모리 관리에 대한 부분을 처리하지 않아도 됩니다.
-
         val binding = MainFragmentBinding.bind(view)
         binding.helloButton.setOnClickListener {
             binding.nameTextView.text = "Hello, Kotlin2"
