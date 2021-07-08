b1b4ba1165d0fab1876e036f659430c5335bcebe
Chansik Yun <chansigi@gmail.com>
2021-07-08 10:58:54 +0900

    .

--- src/24_범위지정함수4.kt
+++ src/24_범위지정함수4.kt
@@ -34,7 +34,8 @@ fun main() {
 
 // use를 사용할 때, 여러개의 자원이 close 호출이 보장되어야 한다면
 // 중첩이 발생할 수 밖에 없습니다.
-
+// https://appmattus.medium.com/effective-kotlin-item-9-prefer-try-with-resources-to-try-finally-aec8c202c30a
+/*
 fun main() {
     try {
         FileOutputStream("a.txt").use { fos ->
@@ -45,6 +46,24 @@ fun main() {
                 }
             }
         }
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+}
+*/
+
+
+fun main() {
+    try {
+        val fos = FileOutputStream("a.txt")
+        val bos = BufferedOutputStream(fos)
+        val dos = DataOutputStream(bos)
+
+        arrayOf(fos, bos, dos).use {
+            dos.writeInt(42)
+            dos.writeUTF("Hello")
+        }
+
     } catch (e: IOException) {
         e.printStackTrace()
     }
@@ -67,3 +86,24 @@ fun main() {
     }
 }
 */
+
+private inline fun <T : Closeable?> Array<T>.use(block: () -> Unit) {
+    var exception: Throwable? = null
+    try {
+        return block()
+    } catch (e: Throwable) {
+        exception = e
+        throw e
+    } finally {
+        when {
+            exception == null -> forEach { it?.close() }
+            else -> forEach {
+                try {
+                    it?.close()
+                } catch (closeException: Throwable) {
+                    // exception.addSuppressed(closeException)
+                }
+            }
+        }
+    }
+}
