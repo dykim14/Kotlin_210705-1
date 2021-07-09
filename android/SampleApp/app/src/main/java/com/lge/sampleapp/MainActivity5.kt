943e36b0fafe1e17ca7f717328f1aba3630f95fd
Chansik Yun <chansigi@gmail.com>
2021-07-09 14:49:40 +0900

    .

--- android/SampleApp/app/src/main/java/com/lge/sampleapp/MainActivity5.kt
+++ android/SampleApp/app/src/main/java/com/lge/sampleapp/MainActivity5.kt
@@ -1,13 +1,19 @@
 package com.lge.sampleapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import coil.load
+import coil.transform.CircleCropTransformation
+import coil.transform.GrayscaleTransformation
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.GsonBuilder
 import com.lge.sampleapp.databinding.MainActivity5Binding
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
@@ -46,19 +52,74 @@ private val retrofit: Retrofit = Retrofit.Builder().apply {
     }.create()
     addConverterFactory(GsonConverterFactory.create(gson))
 
-
 }.build()
 
+// 5. 인터페이스 객체 생성
+val githubApi: GithubApi = retrofit.create(GithubApi::class.java)
+//-------
 
 
 class MainActivity5 : AppCompatActivity() {
+    companion object {
+        const val TAG = "MainActivity5"
+    }
+
     private val binding: MainActivity5Binding by viewBinding()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding.loadButton.setOnClickListener {
+            val call = githubApi.getUser("Google")
+
+            call.enqueue(object : Callback<User> {
+                override fun onResponse(
+                    call: Call<User>,
+                    response: Response<User>
+                ) {
+                    if (response.isSuccessful.not()) {
+                        return
+                    }
+
+                    val user = response.body() ?: return
+                    update(user)
+                }
+
+                override fun onFailure(
+                    call: Call<User>,
+                    t: Throwable
+                ) {
+                    Log.e(TAG, t.localizedMessage, t)
+                }
+
+            })
 
         }
     }
+
+    private fun update(user: User) {
+        runOnUiThread {
+            binding.loginTextView.text = user.login
+
+            binding.avatarImageView.load(user.avatarUrl) {
+                transformations(
+                    CircleCropTransformation(),
+                    GrayscaleTransformation()
+                )
+                crossfade(300)
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
