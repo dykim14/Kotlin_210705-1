b8270b1cd27142b7168d94847da5e6fead523fa6
Chansik Yun <chansigi@gmail.com>
2021-07-09 14:16:37 +0900

    .

--- android/SampleApp/app/src/main/java/com/lge/sampleapp/MainActivity5.kt
+++ android/SampleApp/app/src/main/java/com/lge/sampleapp/MainActivity5.kt
@@ -1,6 +1,17 @@
 package com.lge.sampleapp
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.lge.sampleapp.databinding.MainActivity5Binding
 
-class MainActivity5: AppCompatActivity() {
+class MainActivity5 : AppCompatActivity() {
+    private val binding: MainActivity5Binding by viewBinding()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding.loadButton.setOnClickListener {
+
+        }
+    }
 }
