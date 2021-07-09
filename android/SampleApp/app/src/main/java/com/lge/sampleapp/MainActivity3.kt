7f6f0225c932b60d4a82f68a2803174e2e054d62
Chansik Yun <chansigi@gmail.com>
2021-07-09 10:41:12 +0900

    .

--- android/SampleApp/app/src/main/java/com/lge/sampleapp/MainActivity3.kt
+++ android/SampleApp/app/src/main/java/com/lge/sampleapp/MainActivity3.kt
@@ -1,12 +1,22 @@
 package com.lge.sampleapp
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.viewbinding.ViewBinding
+import com.lge.sampleapp.databinding.MainActivity3Binding
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
 
 class MainActivity3 : AppCompatActivity() {
+
+    private val binding: MainActivity3Binding
+            by ActivityBindingDelegate(MainActivity3Binding::class.java, this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity3)
+
     }
 }
 
@@ -15,3 +25,29 @@ class MainActivity3 : AppCompatActivity() {
 //  => 프로퍼티 위임
 //  => 프로퍼티에 대한 접근자 메소드의 호출을 다른 객체에게 위임한다.(by)
 //  : Android Architecture Sample + AirBnb
+
+class ActivityBindingDelegate<T : ViewBinding>(
+    private val bindingClass: Class<T>,
+    val activity: Activity
+) : ReadOnlyProperty<Activity, T> {
+
+    private var binding: T? = null
+
+    override fun getValue(thisRef: Activity, property: KProperty<*>): T {
+        // 이미 binding 객체가 생성되어 있다면, 생성된 객체를 반환합니다.
+        binding?.let { return it }
+
+        // ViewBinding.inflate(LayoutInflater)
+        val inflateMethod = bindingClass.getMethod(
+            "inflate",
+            LayoutInflater::class.java
+        )
+
+        @Suppress("UNCHECKED_CAST")
+        binding = inflateMethod.invoke(null, thisRef.layoutInflater) as T
+
+        return binding!!
+    }
+}
+
+
