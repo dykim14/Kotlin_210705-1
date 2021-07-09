98be762f7292f1dd908e9eca0d4876532434a219
Chansik Yun <chansigi@gmail.com>
2021-07-09 11:39:34 +0900

    .

---
+++ android/SampleApp/app/src/main/java/com/lge/sampleapp/MainActivity4.kt
@@ -0,0 +1,11 @@
+package com.lge.sampleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity4)
+    }
+}
